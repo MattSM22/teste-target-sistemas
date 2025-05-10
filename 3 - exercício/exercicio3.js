576ee5b705e5380f50ec1675eef347d1eaf15544
Matheus <matheusmagdevelop@gmail.com>
2025-05-10 10:17:06 -0300

    alteração de arquivo de com dados de faturamento do exercicio 3

--- 3 - exercício/exercicio3.js
+++ 3 - exercício/exercicio3.js
@@ -1,14 +1,14 @@
-const faturamentos = require("./faturamento.json");
+const faturamentos = require("./dados.json");
 
 const menorValor = faturamentos
-  .filter(faturamento => faturamento.valor > 0 && faturamento.status !== 'Feriado' && faturamento.status !== 'Sem Operação')
+  .filter(faturamento => faturamento.valor > 0)
   .reduce((valorMenor, valorAtual) => valorAtual.valor < valorMenor.valor ? valorAtual : valorMenor);
 
 const maiorValor = faturamentos
-  .filter(faturamento => faturamento.valor > 0 && faturamento.status !== 'Feriado' && faturamento.status !== 'Sem Operação')
+  .filter(faturamento => faturamento.valor > 0)
   .reduce((valorMaior, valorAtual) => valorAtual.valor > valorMaior.valor ? valorAtual : valorMaior);
 
-const faturamentosValidos = faturamentos.filter(f => f.valor > 0 && f.status !== 'Feriado' && f.status !== 'Sem Operação');
+const faturamentosValidos = faturamentos.filter(f => f.valor > 0);
 
 if (faturamentosValidos.length > 0) {
   const somaTotal = faturamentosValidos.reduce((soma, atual) => soma + atual.valor, 0);
